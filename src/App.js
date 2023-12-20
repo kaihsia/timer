dabeffeaa833bbde958db0949df55acaf0801bdb
Kai H <kaih@Kais-Mac-mini.local>
2023-12-20 10:51:00 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [ timer, setTimer ] = useState(0)
+  const [ start, setStart ] = useState(false)
+
+  useEffect(() => {
+    if (start) {
+      const interval = setInterval(() => {
+        setTimer(prevState => prevState + 1)
+      }, 1000)
+  
+      return () => clearInterval(interval)
+    }
+  }, [start, timer])
+
+  const handleStart = () => {
+    setStart(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{timer}</p>
+      <button onClick={handleStart}>Start</button>
     </div>
   );
 }
